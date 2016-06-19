3db42353d57b2019564a4b71a575456571512aec
ShadyF <shadster09@gmail.com>
2016-06-19 22:17:47 +0200

    Added Tabs at the bottom of editor html + some design fixes

--- ImageEditor/static/js/editor.js
+++ ImageEditor/static/js/editor.js
@@ -1,18 +1,16 @@
 $(function () {
     "use strict";
-    /*$('#imageLoader').on('change', handleImage);
+    $('#imageLoader').on('change', handleImage);
     $(".filter-button").click(processImage);
     $(".image-forms").on('submit', processImage)
     var canvas = document.getElementById('imageCanvas');
-     var ctx = canvas.getContext('2d');*/
+    var ctx = canvas.getContext('2d');
 
     function handleImage(event) {
         var reader = new FileReader();
         reader.onload = function (theFile) {
             var img = new Image();
             img.onload = function () {
-                canvas.width = img.width;
-                canvas.height = img.height;
                 ctx.drawImage(img, 0, 0);
             };
             img.src = theFile.target.result;
@@ -35,9 +33,15 @@ $(function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         var image = new Image();
         image.src = json.processed_image;
-        canvas.height = image.height;
-        canvas.width = image.width;
+        if (image.width > canvas.width || image.height > canvas.height)
+            fitToContainer(canvas);
+        else {
+
+            canvas.height = image.height;
+            canvas.width = image.width;
+        }
         ctx.drawImage(image, 0, 0);
+
     }
 
     $("#menu-toggle").click(function (e) {
@@ -45,4 +49,23 @@ $(function () {
         $("#wrapper").toggleClass("toggled");
     });
 
+    $('.nav-tabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show')
+    });
+    function fitToContainer(canvas) {
+        // Make it visually fill the positioned parent
+        console.log(canvas.style.width);
+        canvas.style.width = '100%';
+        canvas.style.height = '100%';
+        // ...then set the internal size to match
+        canvas.width = canvas.style.width;
+        canvas.height = canvas.style.height;
+    }
+
+    window.onload = function () {
+        var div = document.getElementsByClassName("image-canvas")
+        canvas.height = div[0].offsetHeight;
+        canvas.width = div[0].offsetWidth;
+    }
 });
