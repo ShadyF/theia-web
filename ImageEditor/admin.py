424fb54cd75fd1920f5cf465d1f265c17e87c8ba
ShadyF <shadster09@gmail.com>
2016-06-24 14:58:47 +0200

    Added model for Image Operations

--- ImageEditor/admin.py
+++ ImageEditor/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import ColorTint
+from .models import ColorTint, ImageFunction
 
 admin.site.register(ColorTint)
+admin.site.register(ImageFunction)
--- ImageEditor/models.py
+++ ImageEditor/models.py
@@ -9,3 +9,33 @@ class ColorTint(models.Model):
 
     def __str__(self):
         return self.name + " (" + str(self.red) + ", " + str(self.green) + ", " + str(self.blue) + ")"
+
+
+class ImageFunction(models.Model):
+    FUNCTION_TYPES = (
+        ('Transform', 'Transform'),
+        ('Tint', 'Color Tint'),
+        ('Adjustment', 'Adjustment'),
+        ('ColorFilter', 'Color Filter'),
+        ('KernelFilter', 'Kernel Filter')
+    )
+    display_name = models.CharField(max_length=30, unique=True)
+    class_name = models.CharField(max_length=30)
+    function_type = models.CharField(max_length=15, choices=FUNCTION_TYPES)
+
+    def __str__(self):
+        return "[" + self.function_type + "] " + "'" + self.display_name + "': " + self.class_name
+
+        # class Meta:
+        #     abstract = True
+
+# class TransformFunction(ImageFunction):
+#     pass
+#
+#
+# class ColorFilterFunction(ImageFunction):
+#     pass
+#
+#
+# class KernelFilterFunction(ImageFunction):
+#     pass
