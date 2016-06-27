df1064584183a68188d1ca86e9538eb79e3b7e76
ShadyF <shadster09@gmail.com>
2016-06-27 21:30:07 +0200

    Added session data to the admin interface

--- ImageEditor/admin.py
+++ ImageEditor/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import ColorTint, ImageFunction
-
+from django.contrib.sessions.models import Session
 admin.site.register(ColorTint)
 admin.site.register(ImageFunction)
+
+
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return obj.get_decoded()
+
+    list_display = ['session_key', '_session_data', 'expire_date']
+
+
+admin.site.register(Session, SessionAdmin)
