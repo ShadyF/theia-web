e5949ad03afc66322f381609cdfffa4ee93dfda2
ShadyF <shadster09@gmail.com>
2016-06-28 02:46:03 +0200

    Refactored and re-organized editor.js

--- ImageEditor/static/js/editor.js
+++ ImageEditor/static/js/editor.js
@@ -1,171 +1,28 @@
-/*TODO: Clean this mess up and fix enhancement buttons needing two clicks to trigger*/
+/*TODO: Fix enhancement buttons needing two clicks to trigger*/
 /*TODO: Seperate everything*/
+/*TODO: Save image after drawing */
 $(function () {
     "use strict";
-    $('#imageLoader').on('change', uploadImageFromForm);
-    $(".filter-button").click(processImage);
-    $(".tint").click(applyTint);
-    $(".image-forms").on('submit', processImage);
+
     var canvas = document.getElementsByTagName("canvas")[0];
     var ctx = canvas.getContext('2d');
     var current_image = new Image();
-    var input_slider = '<span class="EnhancementValue">1.0</span>' +
-        '<input class="EnhancementFilter" type="range" min="-1.0" max="4.0" step="0.1" value="1.0">';
+    var draw = false;
+    var canvas_wrapper = $("#canvas-wrapper")[0];
+    var all_popovers = $('[data-toggle="popover"]');
 
-    $('[data-toggle="popover"]').popover({
-        placement: "top", html: true,
-        content: input_slider
-    });
-
-    $('body').on('click', function (e) {
-        $('[data-toggle="popover"]').each(function () {
-            //the 'is' for buttons that trigger popups
-            //the 'has' for icons within a button that triggers a popup
-            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
-                $(this).popover('hide');
-            }
-        });
-    });
-
-    /*TODO: Optimize some of this stuff here*/
-
-    $('[data-toggle="popover"]').on('shown.bs.popover', function () {
-        $("input[type=range]").data('enhancement', $(this).data('enhancement'));
-        $("input[type=range]").on('input', function () {
-            $(".EnhancementValue").text($(this).val())
-        });
-        $("input[type=range]").on('change', function () {
-            event.preventDefault();
-            var dataURL = canvas.toDataURL();
-            $.post($(this).data('enhancement') + "/", {
-                imgBase64: dataURL,
-                params: $(this).val()
-            }, handleCanvasImage);
-        })
-    });
+    /*###############################################
+     ##        DRAWING RELATED FUNCTIONS          ##
+     ###############################################*/
 
     $(canvas).jqScribble({width: 0, height: 0, draw: false, brushSize: 4});
 
-    function uploadImageFromForm(event) {
-        var reader = new FileReader();
-        reader.onload = function (theFile) {
-            var img = new Image();
-            $.post("upload/", {
-                imgBase64: theFile.target.result
-            });
-            img.onload = function () {
-                current_image = img;
-                redrawCanvas()
-            };
-            img.src = theFile.target.result;
-            current_image = img;
-        };
-        reader.readAsDataURL(event.target.files[0]);
-    }
-
-    function applyTint(event) {
-        var dataURL = canvas.toDataURL();
-        $.post($(this).data('operation') + "/", {
-            imgBase64: dataURL,
-            params: $(this).data('tint_name')
-        }, handleCanvasImage);
-    }
-
-    function saveImageOnServer(img_data) {
-        $.post("process/", {
-            imgBase64: theFile.target.result,
-            action: $(this).data("action"),
-            save: true,
-            params: $(this).serialize()
-        });
-    }
-
-    function handleImage(event) {
-        var reader = new FileReader();
-        reader.onload = function (theFile) {
-            var img = new Image();
-            img.onload = function () {
-                canvas.width = img.width;
-                canvas.height = img.height;
-                ctx.drawImage(img, 0, 0);
-            };
-            img.src = theFile.target.result;
-        };
-        reader.readAsDataURL(event.target.files[0]);
-    }
-
-    function processImage(event) {
-        event.preventDefault();
-        var dataURL = canvas.toDataURL();
-        $.post("process/", {
-            imgBase64: dataURL,
-            action: $(this).data("action"),
-            params: $(this).serialize()
-        }, handleCanvasImage);
-    }
-
-    function handleCanvasImage(json) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        var canvas_wrapper = $(".image-canvas")[0];
-        var image = new Image();
-        image.src = json.processed_image;
-        current_image = image;
-        var divHeight = canvas_wrapper.clientHeight;
-        var divWidth = canvas_wrapper.clientWidth;
-        var yScale = divHeight / image.height;
-        var xScale = divWidth / image.width;
-
-        var newImgHeight = image.height;
-        var newImgWidth = image.width;
-
-        if (newImgHeight > divHeight) {
-            newImgHeight = image.height * xScale;
-            newImgWidth = divWidth;
-        }
-        else if (newImgWidth > divWidth) {
-            newImgHeight = divHeight;
-            newImgWidth = image.width * yScale;
-            canvas.width = newImgWidth;
-            canvas.height = newImgHeight;
-        }
-        canvas.width = newImgWidth;
-        canvas.height = newImgHeight;
-        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-    }
-
-    $(".menu-toggle").click(function (e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-        $(this).toggleClass("toggled");
-        $(this).toggleClass('menu-toggle-active');
+    $('#color-picker').colorpicker({align: 'left'}).on('changeColor', function (e) {
+        $(".glyphicon-tint")[0].style.color = e.color.toHex();
+        $(canvas).data('jqScribble').update({brushColor: e.color.toString('rgb')})
     });
 
-    $('.nav-tabs a').click(function (e) {
-        e.preventDefault();
-        $(this).tab('show')
-    });
-    function fitToContainer(canvas) {
-        // Make it visually fill the positioned parent
-        console.log(canvas.style.width);
-        canvas.style.width = '100%';
-        canvas.style.height = '100%';
-        // ...then set the internal size to match
-        canvas.width = canvas.style.width;
-        canvas.height = canvas.style.height;
-    }
-
-    window.onload = function () {
-        var rect = canvas.parentNode.getBoundingClientRect();
-        canvas.width = rect.width - 15;
-        canvas.height = rect.height - 15;
-        current_image.height = canvas.height;
-        current_image.width = canvas.width;
-    };
-
-
-    var draw = false;
-
-    $('button#draw').click(function () {
+    $('#draw-button').click(function () {
         $(canvas).data('jqScribble').update({draw: !draw});
         if (!draw)
             $(canvas).css('cursor', 'crosshair');
@@ -174,19 +31,86 @@ $(function () {
         draw = !draw;
     });
 
-    $('#cp4').colorpicker({align: 'left'}).on('changeColor', function (e) {
-        $(".glyphicon-tint")[0].style.color = e.color.toHex();
-        $(canvas).data('jqScribble').update({brushColor: e.color.toString('rgb')})
-    });
-    $(".nav-tabs").click(function () {
-        console.log($($(".active").children('a')[0]).css('backgound-color'));
-        $(this).css.borderTopColor = $(".active").css.background
+    /*###############################################*/
+
+    $('#imageLoader').on('change', uploadImageFromForm);
+    $(window).on('resize', redrawCanvas);
+    $(".tint").click(function () {
+        requestImageOperation($(this).data('operation') + '/', $(this).data('tint_name'));
     });
 
-    $(".menu-toggle").on('click', function () {
-        $("#wrapper").one(' otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+    $('.nav-tabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show')
+    });
+
+    all_popovers
+        .popover({
+            placement: "top", html: true
+        })
+        .on('shown.bs.popover', function () {
+            var slider = $('.AdjustmentSlider');
+            slider.on('input', function () {
+                $('.AdjustmentValue').text($(this).val())
+            });
+            slider.on('change', function (e) {
+                e.preventDefault();
+                requestImageOperation($(this).data('operation') + "/", $(this).val());
+            });
+        });
+
+
+    $('body').on('click', function (e) {
+        all_popovers.each(function () {
+            //the 'is' for buttons that trigger popups
+            //the 'has' for icons within a button that triggers a popup
+            if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+                $(this).popover('hide');
+            }
+        });
+    });
+
+    function uploadImageFromForm(event) {
+        var reader = new FileReader();
+        reader.onload = function (theFile) {
+            var img = new Image();
+            img.src = theFile.target.result;
+
+            $.post("upload/", {
+                imgBase64: theFile.target.result
+            });
+            img.onload = function () {
+                current_image = img;
+                redrawCanvas()
+            };
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    }
+
+
+    function requestImageOperation(op_url, op_params) {
+        var dataURL = canvas.toDataURL();
+        var op_data = {
+            imgBase64: dataURL,
+            params: op_params
+        };
+
+        $.ajax({
+            url: op_url,
+            type: 'POST',
+            data: op_data,
+            success: updateCurrentImage
+        });
+    }
+
+    $(".menu-toggle").click(function (e) {
+        e.preventDefault();
+        var wrapper = $('#wrapper');
+        wrapper.toggleClass("toggled");
+        $(this).toggleClass('menu-toggle-active');
+
+        wrapper.one(' otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            var canvas_wrapper = $(".image-canvas")[0];
             var divHeight = canvas_wrapper.clientHeight;
             var divWidth = canvas_wrapper.clientWidth;
             var yScale = divHeight / current_image.height;
@@ -208,10 +132,31 @@ $(function () {
             ctx.drawImage(current_image, 0, 0, canvas.width, canvas.height);
         })
     });
-    $(window).on('resize', redrawCanvas);
+
+    window.onload = function () {
+        var rect = canvas.parentNode.getBoundingClientRect();
+        canvas.width = rect.width - 15;
+        canvas.height = rect.height - 15;
+        current_image.height = canvas.height;
+        current_image.width = canvas.width;
+    };
+
+
+    /* TODO: Make tab panes have the same background color as their nav tab links */
+    $(".nav-tabs").click(function () {
+        console.log($($(".active").children('a')[0]).css('backgound-color'));
+        $(this).css.borderTopColor = $(".active").css.background
+    });
+
+    function updateCurrentImage(json) {
+        var image = new Image();
+        image.src = json.processed_image;
+        current_image = image;
+        redrawCanvas();
+    }
+
     function redrawCanvas() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        var canvas_wrapper = $(".image-canvas")[0];
         var divHeight = canvas_wrapper.clientHeight;
         var divWidth = canvas_wrapper.clientWidth;
         var yScale = divHeight / current_image.height;
@@ -228,8 +173,8 @@ $(function () {
             newImgHeight = divHeight;
             newImgWidth = current_image.width * yScale;
         }
-        canvas.width = newImgWidth - 15;
-        canvas.height = newImgHeight - 15;
+        canvas.width = newImgWidth;
+        canvas.height = newImgHeight;
         ctx.drawImage(current_image, 0, 0, canvas.width, canvas.height);
     }
 });
