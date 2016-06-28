dc252e43ac7fe3862eb48363c1d49569f1f9a922
ShadyF <shadster09@gmail.com>
2016-06-28 20:03:54 +0200

    Added a color filter! (Ashes)

--- ImageEditor/ColorFilters.py
+++ ImageEditor/ColorFilters.py
@@ -1,43 +1,41 @@
 from PIL import Image
 from subprocess import check_output, STDOUT
-
+from .models import ColorFilter as color_filer_model
+from django.shortcuts import get_object_or_404
+from tempfile import NamedTemporaryFile
 
 class FilterManager:
-    pass
+    def __init__(self, filter_name):
+        filter_model = get_object_or_404(color_filer_model, name=filter_name)
+        self.filter_to_be_applied = filter_model.class_name
+        print(self.filter_to_be_applied)
+
+    def process(self, image):
+        temp_image = NamedTemporaryFile(mode='w+b')
+        image.save(temp_image, format(image.format))
+        chosen_filter = globals()[self.filter_to_be_applied]()
+        chosen_filter.apply(temp_image.name)
+        image = Image.open(temp_image)
+        return image
 
 
 class ColorFilter:
-    def __init__(self, filename):
-        self.filename = filename
-        self.im = False
-
-    def image(self):
-        if not self.im:
-            self.im = Image.open(self.filename)
-        return self.im
-
     def execute(self, command, **kwargs):
-        default = dict(
-            filename=self.filename,
-            width=self.image().size[0],
-            height=self.image().size[1]
-        )
-        format = dict(default.items() + kwargs.items())
+        format = dict(kwargs.items())
         command = command.format(**format)
         error = check_output(command, shell=True, stderr=STDOUT)
         return error
 
-    def colortone(self, color, level, type=0):
-
+    def colortone(self, color, level, apply_negation=True):
         arg0 = level
         arg1 = 100 - level
-        if type == 0:
-            negate = '-negate'
-        else:
-            negate = ''
+        negate = '-negate' if apply_negation else ''
 
         self.execute(
-            "convert {filename} \( -clone 0 -fill '{color}' -colorize 100% \) \( -clone 0 -colorspace gray {negate} \) -compose blend -define compose:args={arg0},{arg1} -composite {filename}",
+            "convert {filename} \
+            ( -clone 0 -fill '{color}' -colorize 100% \) \
+            ( -clone 0 -colorspace gray {negate})\
+             -compose blend -define compose:args={arg0},{arg1} -composite {filename}",
             color=color,
             negate=negate,
             arg0=arg0,
@@ -45,5 +43,7 @@ class ColorFilter:
         )
 
 
-class Lomo(ColorFilter):
-    pass
+class Ashes(ColorFilter):
+    def apply(self, file_path):
+        self.execute("convert {filename} -modulate 120,10,100 -fill '#222b6d' -colorize 20\
+                     -gamma 0.5 -contrast -contrast {filename}", filename=file_path)
